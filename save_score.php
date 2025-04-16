8784e740a3867cc4f15e9dc3a27f7a4dcfe4281e
Sandor Barnabas <h378211@stud.u-szeged.hu>
2025-04-16 20:13:20 +0200

    Little fix

--- save_score.php
+++ save_score.php
@@ -3,10 +3,9 @@ $db = new PDO('sqlite:scores.db');
 $db->exec("CREATE TABLE IF NOT EXISTS highscores (id INTEGER PRIMARY KEY, player_name TEXT, score INTEGER, date TEXT)");
 $stmt = $db->prepare("INSERT INTO highscores (player_name, score, date) VALUES (:name, :score, :date)");
 $safePlayerName = escapeshellarg($_POST['player_name']);
-$safeScore = escapeshellarg($_POST['score']);
 $stmt->execute([
     ':name' => $safePlayerName,
-    ':score' => (int)$safeScore,
+    ':score' => (int)$_POST['score'],
     ':date' => date('Y-m-d H:i:s')
 ]);
 ?>
