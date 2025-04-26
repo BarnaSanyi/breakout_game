dc2ca557c250a50dfb22067663422ce85feee1c0
Sandor Barnabas <h378211@stud.u-szeged.hu>
2025-04-26 23:03:13 +0200

    favicon added, change from POST to GET

--- save_score.php
+++ save_score.php
@@ -2,10 +2,10 @@
 $db = new PDO('sqlite:scores.db');
 $db->exec("CREATE TABLE IF NOT EXISTS highscores (id INTEGER PRIMARY KEY, player_name TEXT, score INTEGER, date TEXT)");
 $stmt = $db->prepare("INSERT INTO highscores (player_name, score, date) VALUES (:name, :score, :date)");
-$safePlayerName = escapeshellarg($_POST['player_name']);
+$safePlayerName = escapeshellarg($_GET['player_name']);
 $stmt->execute([
     ':name' => $safePlayerName,
-    ':score' => (int)$_POST['score'],
+    ':score' => (int)$_GET['score'],
     ':date' => date('Y-m-d H:i:s')
 ]);
 ?>
